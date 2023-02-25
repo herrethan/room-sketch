1c299ca81f1a3dd3c330c0869dc1d28105cf0ccc
Ethan Herr <eherr@reonomy.com>
2023-02-25 12:48:46 -0500

    adds storybook with mock components

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  refs: {
+    '@chakra-ui/react': {
+      disable: true,
+    },
+  },
+}
