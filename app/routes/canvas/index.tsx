8b06632bb3bbb889963b87dace67ed1002529036
Ethan Herr <eherr@reonomy.com>
2023-03-12 09:42:29 -0400

    undo ugly pretty

--- app/routes/canvas/index.tsx
+++ app/routes/canvas/index.tsx
@@ -2,31 +2,12 @@ import Editor from '~/components/editor';
 import { EditorProvider } from '~/components/editor/provider';
 import type { Wall } from '~/data/walls';
 
+// prettier-ignore
 const someWalls: Wall[] = [
-  {
-    position: [
-      [-5, -3],
-      [-5, 3],
-    ],
-  },
-  {
-    position: [
-      [-5, 3],
-      [5, 3],
-    ],
-  },
-  {
-    position: [
-      [5, 3],
-      [5, -3],
-    ],
-  },
-  {
-    position: [
-      [5, -3],
-      [-5, -3],
-    ],
-  },
+  { position: [[-5, -3], [-5, 3]] },
+  { position: [[-5, 3], [5, 3]] },
+  { position: [[5, 3], [5, -3]] },
+  { position: [[5, -3], [-5, -3]] },
 ];
 
 export default function Canvas() {
